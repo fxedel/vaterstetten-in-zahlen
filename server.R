905637607a7ff332d68b450f663203c39d3d71c6
Felix Edelmann <fxedel@gmail.com>
2021-01-26 15:41:06 +0100

    Enable URL bookmarking to remember current tab

--- server.R
+++ server.R
@@ -4,28 +4,38 @@ library(ggplot2)
 
 theme_set(theme_light())
 
-ui <- dashboardPage(skin = "purple",
-  dashboardHeader(
-    title = "Vaterstetten in Zahlen",
-    titleWidth = 250
-  ),
-  dashboardSidebar(
-    width = 250,
-    sidebarMenu(id = "sidebar",
-      menuItem("Start", tabName = "start", icon = icon("home"))
-    )
-  ),
-  dashboardBody(
-    tabItems(
-      tabItem(tabName = "start",
-        h2("Start")
+ui <- function(request) {
+  dashboardPage(skin = "purple",
+    dashboardHeader(
+      title = "Vaterstetten in Zahlen",
+      titleWidth = 250
+    ),
+    dashboardSidebar(
+      width = 250,
+      sidebarMenu(id = "sidebar",
+        menuItem("Start", tabName = "start", icon = icon("home"))
+      )
+    ),
+    dashboardBody(
+      tabItems(
+        tabItem(tabName = "start",
+          h2("Start")
+        )
       )
     )
   )
-)
-
-server <- function(input, output, session) {
-  
 }
 
-shinyApp(ui, server, options = list(host = "127.0.0.1", port = 4373))
+server <- function(input, output, session) {
+  observe({
+    # Trigger this observer every time an input changes
+    reactiveValuesToList(input)
+    session$doBookmark()
+  })
+  onBookmarked(function(url) {
+    updateQueryString(url)
+  })
+
+}
+
+shinyApp(ui, server, options = list(host = "127.0.0.1", port = 4373), enableBookmarking = "url")
